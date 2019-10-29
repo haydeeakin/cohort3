99a7bfad4c9e9aa2e3a039c507c565c313f5145d
haydeeakin <haydeeperalta@Haydees-MacBook-Pro-2.local>
2019-10-29 00:33:59 -0600

    finished DOM & working on test

--- src/02-dom/scripts/functions.js
+++ src/02-dom/scripts/functions.js
@@ -1,10 +1,58 @@
 
 
 const functions = {
-    thekids: (event) => {
-        console.log(event.target.parentElement.children[2])
+    
+    // Basic DOM
+    theKids: () => {
+        console.log(event.target.parentElement.children[2]);
+    },
+    newLi: () => {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode("This new item");
+        node.appendChild(textnode);
+        document.getElementById("list").appendChild(node);
+        
+    },
+    
+    // Working with cards
+ 
+    cardObject: {
+            count: 0,
+            addCard: () => {
+                functions.cardObject.count++;
+                const newDiv = document.createElement("div");
+                newDiv.setAttribute("class", "card");
+                newDiv.setAttribute("id", functions.cardObject.count);
+                let newDivText = document.createTextNode("Card" + functions.cardObject.count);
+                let newDivAddButton = document.createElement("button");
+                newDivAddButton.textContent = "Add Before";
+                let newDivAfterButton = document.createElement("button");
+                newDivAfterButton.textContent = "Add After";
+                let newDivDeleteButton = document.createElement("button");
+                newDivDeleteButton.textContent = "Delete";
+                newDiv.appendChild(newDivText);
+                newDiv.appendChild(newDivAddButton);
+                newDiv.appendChild(newDivAfterButton);
+                newDiv.appendChild(newDivDeleteButton);
+                return newDiv;
+
+            },
+    },
+    mainAdd: (main) => {
+        main.appendChild(functions.cardObject.addCard());
+    },
+    
+    addBefore: (main, card) => {
+        main.insertBefore(functions.cardObject.addCard(), card);
+    },
+
+    addAfter: (main, card) => {
+        main.insertBefore(functions.cardObject.addCard(), card.nextSibling);
+    },
+
+    deleteLast: (card) => {
+        card.remove();
     }
-
 };
-
+// 
 export default functions
--- src/02-dom/scripts/index.js
+++ src/02-dom/scripts/index.js
@@ -1,14 +1,30 @@
 
 import functions from './functions.js';
 
+// Basic DOM //
 basic.addEventListener("click", () => {
-    console.log(event.target)
+    console.log(event.target);
 });
 
-show.addEventListener("click", functions.thekids);
-add.addEventListener("click", functions.newli);
+show.addEventListener("click", functions.theKids);
+add.addEventListener("click", functions.newLi);
 
+// Working with Cards //
+maincard.addEventListener("click", () => {
+    switch(event.target.textContent) {
+        case "Add Card":
+            functions.mainAdd(maincard);
+            break;
+        case "Add Before":
+            functions.addBefore(maincard, event.target.parentElement);
+            break;
+        case "Add After":
+            functions.addAfter(maincard, event.target.parentElement);
+            break;
+        case "Delete":
+            functions.deleteLast(event.target.parentElement);
+            break;
+    }
 
+});
 
-
-  
