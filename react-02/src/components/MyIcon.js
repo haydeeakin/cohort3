cc8445a48a2ec0bb0cae4bde0f8cddc974e059b4
haydeeakin <peraltavazquezhaydee@gmail.com>
2020-01-28 22:43:14 -0700

    added icons

---
+++ react-02/src/components/MyIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class MyIcon extends React.Component {
+  render() {
+    return (
+      <img
+        className="Icon Icon-logo"
+        src={this.props.src}
+        alt={this.props.name}
+      />
+    );
+  }
+}
+
+export default MyIcon;
